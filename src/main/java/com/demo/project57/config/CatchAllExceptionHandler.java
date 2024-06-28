0325cca90871e598a8b69ea070fa0721537d6aef
Arjun Surendra <asurendra@vmware.com>
2024-06-28 10:48:45 +0530

    code

--- src/main/java/com/demo/project57/config/CatchAllExceptionHandler.java
+++ src/main/java/com/demo/project57/config/CatchAllExceptionHandler.java
@@ -4,9 +4,9 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestController
+@RestControllerAdvice
 @Order(9999)
 public class CatchAllExceptionHandler {
     @ExceptionHandler({RuntimeException.class})
